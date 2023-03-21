4c2da11d96622f4658b9e5f721e5385ccd1cd7a3
jonathan <jonathan@DELL-LATITUDE>
2023-03-21 14:33:19 +0000

    Changed channel name regex

--- M3UToolsJPA/src/main/java/com/hawkins/m3utoolsjpa/regex/Patterns.java
+++ M3UToolsJPA/src/main/java/com/hawkins/m3utoolsjpa/regex/Patterns.java
@@ -19,6 +19,7 @@ public class Patterns {
 	public static final Pattern PIPE_REGEX = Pattern.compile("([^|]*\\|{1}[^|]{1}[^|])", Pattern.CASE_INSENSITIVE);
 	public static final Pattern ADULT_REGEX = Pattern.compile("\b(?:ADULT|For Adults)\b", Pattern.CASE_INSENSITIVE);
 	public static final Pattern HASH_REGEX = Pattern.compile("[#]{3}");
-	public static final Pattern VALID_CHANNEL_NAME = Pattern.compile("[^a-zA-Z0-9 -]");
+	public static final Pattern VALID_CHANNEL_NAME = Pattern.compile("(?:\\w[A-Z]\\s[-]\\s)");
+	//public static final Pattern VALID_CHANNEL_NAME = Pattern.compile("[^a-zA-Z0-9 -]");
 	public static final Pattern STRIP_COUNTRY_IDETIFIER = Pattern.compile(".{2,} - *");
 }
