05cf1aa388d6121181b2c7d59529db2c4f4916f6
jonathan <jonathan@DELL-LATITUDE>
2023-07-27 11:50:22 +0100

    Implementing Criteria Updates and Deletes

---
+++ M3UToolsJPA/src/main/java/com/hawkins/m3utoolsjpa/service/DatabaseService.java
@@ -0,0 +1,40 @@
+package com.hawkins.m3utoolsjpa.service;
+
+import org.springframework.stereotype.Service;
+
+import com.hawkins.m3utoolsjpa.data.M3UGroup;
+import com.hawkins.m3utoolsjpa.data.M3UItem;
+
+import jakarta.persistence.EntityManager;
+import jakarta.persistence.PersistenceContext;
+import jakarta.persistence.criteria.CriteriaBuilder;
+import jakarta.persistence.criteria.CriteriaDelete;
+import jakarta.persistence.criteria.Root;
+import jakarta.transaction.Transactional;
+import lombok.extern.slf4j.Slf4j;
+
+@Service
+@Slf4j
+@Transactional
+public class DatabaseService {
+
+	@PersistenceContext
+	private EntityManager em;
+	
+	public void DeleteItemsAndGroups() {
+		
+		CriteriaBuilder cb = em.getCriteriaBuilder();
+		
+		CriteriaDelete<M3UItem> deleteItems = cb.createCriteriaDelete(M3UItem.class);
+		
+		Root ei = deleteItems.from(M3UItem.class);
+		
+		this.em.createQuery(deleteItems).executeUpdate();
+		
+		CriteriaDelete<M3UGroup> deleteGroups = cb.createCriteriaDelete(M3UGroup.class);
+		
+		Root eg = deleteGroups.from(M3UGroup.class);
+		
+		this.em.createQuery(deleteGroups).executeUpdate();
+	}
+}
