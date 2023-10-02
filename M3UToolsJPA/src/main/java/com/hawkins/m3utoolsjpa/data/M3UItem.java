22d03ebbca91c6ac71e9477533175978b9cb54d8
duckulaj <jhawkins63@hotmail.com>
2023-10-02 21:02:28 +0100

    Bootstrap 5 work in progress

--- M3UToolsJPA/src/main/java/com/hawkins/m3utoolsjpa/data/M3UItem.java
+++ M3UToolsJPA/src/main/java/com/hawkins/m3utoolsjpa/data/M3UItem.java
@@ -4,6 +4,7 @@ import org.hibernate.annotations.GenericGenerator;
 import org.hibernate.annotations.Parameter;
 
 import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.Id;
 import jakarta.persistence.Index;
@@ -31,7 +32,7 @@ public class M3UItem {
 	        }
 	)
 	
-	@ManyToOne
+	@ManyToOne(fetch=FetchType.LAZY)
     @JoinColumn(name="groupId", nullable=false, insertable=false, updatable=false)
     private M3UGroup group;
 
