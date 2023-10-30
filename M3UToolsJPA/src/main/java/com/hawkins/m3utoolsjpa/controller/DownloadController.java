39b0b3e3d42ec06d2c0526962559f6300fe59f6a
duckulaj <jhawkins63@hotmail.com>
2023-10-30 00:09:31 +0000

    Implemented getting file download size

--- M3UToolsJPA/src/main/java/com/hawkins/m3utoolsjpa/controller/DownloadController.java
+++ M3UToolsJPA/src/main/java/com/hawkins/m3utoolsjpa/controller/DownloadController.java
@@ -7,11 +7,14 @@ import java.net.URISyntaxException;
 import java.net.URL;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpHeaders;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
 import com.hawkins.m3utoolsjpa.data.M3UItemRepository;
+import com.hawkins.m3utoolsjpa.properties.DownloadProperties;
+import com.hawkins.m3utoolsjpa.utils.NetUtils;
 import com.hawkins.m3utoolsjpa.utils.Utils;
 
 import jakarta.servlet.http.HttpServletResponse;
@@ -27,21 +30,27 @@ public class DownloadController {
 	@GetMapping(value ="newDownload", params = { "name" })
     public void newDownload(@RequestParam String name, HttpServletResponse response) throws IOException {
         
+		Long contentSize = 0L;
         URL url = null;
 		try {
 			url = new URI(Utils.getURLFromName(name, itemRepository)).toURL();
 			log.info("Downloading {}", name);
+			contentSize = NetUtils.getContentSizeFromUrl(url);
 		} catch (MalformedURLException e) {
 			e.printStackTrace();
 		} catch (URISyntaxException e) {
 			e.printStackTrace();
 		}
         
-		int bufferSize = 16384;
+		int bufferSize = DownloadProperties.getInstance().getBufferSize();
         BufferedInputStream inputStream = new BufferedInputStream(url.openStream(), bufferSize);
 
         response.setContentType("application/octet-stream");
         response.setHeader("Content-Disposition", "attachment; filename=" + name + ".mp4");
+        response.setHeader("Content-Length", contentSize.toString());
+        response.addHeader(HttpHeaders.CONNECTION, "keep-alive");
+        // response.addHeader("Pragma", "no-cache");
+        // response.addHeader("Cache-Control", "no-cache");
 
         
         int bytesRead;
