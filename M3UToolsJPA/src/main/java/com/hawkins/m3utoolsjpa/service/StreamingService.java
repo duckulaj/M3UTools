c50983fb0670166b7a268b56fd1e804d2d8af6c2
jonathan <jonathan@DELL-MINT>
2024-02-07 11:32:39 +0000

    Refactoring of StreamingService

--- M3UToolsJPA/src/main/java/com/hawkins/m3utoolsjpa/service/StreamingService.java
+++ M3UToolsJPA/src/main/java/com/hawkins/m3utoolsjpa/service/StreamingService.java
@@ -1,5 +1,6 @@
 package com.hawkins.m3utoolsjpa.service;
 
+import java.io.ByteArrayOutputStream;
 import java.io.IOException;
 import java.io.InputStream;
 import java.net.HttpURLConnection;
@@ -18,64 +19,82 @@ import com.hawkins.m3utoolsjpa.utils.NetUtils;
 @Service
 public class StreamingService {
 
-	private static final long ChunkSize = 1000000L;
+	private static final long ChunkSize = 10000L;
 
-	public ResourceRegion streamVideo(String streamUrl, HttpHeaders headers) {
+	public byte[] getVideo(String streamUrl, String range, long fileSize) {
+		byte[] data = null;
 
-		UrlResource video = null;
-		try {
-			video = new UrlResource("url:" + streamUrl);
-		} catch (MalformedURLException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}
-		ResourceRegion region = null;
-		try {
-			region = resourceRegion(video, headers);
-		} catch (IOException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}
-		
-		return region;
-		
-	}
-
-	private ResourceRegion resourceRegion(UrlResource video, HttpHeaders headers) throws IOException {
-		long contentLength = NetUtils.getContentSizeFromUrl(video.getURL());
-		HttpRange range = headers.getRange().stream().findFirst().orElse(null);
-		if (range != null) {
-			long start = range.getRangeStart(contentLength);
-			long end = range.getRangeEnd(contentLength);
-			long rangeLength = Math.min(ChunkSize, end - start + 1);
-			return new ResourceRegion(video, start, rangeLength);
+		String[] ranges = range.split("-");
+		Long rangeStart = Long.parseLong(ranges[0].substring(6));
+		Long rangeEnd = fileSize;
+		if (ranges.length > 1) {
+			rangeEnd = Long.parseLong(ranges[1]);
 		} else {
-			long rangeLength = Math.min(ChunkSize, contentLength);
-			return new ResourceRegion(video, 0, rangeLength);
+			rangeEnd = fileSize - 1;
 		}
-	}
-	
-	public InputStream getInputStream(String streamUrl, HttpHeaders headers) {
-		
-		HttpURLConnection con;
-		InputStream targetStream = null;
+		if (fileSize < rangeEnd) {
+			rangeEnd = fileSize - 1;
+		}
+		String contentLength = String.valueOf((rangeEnd - rangeStart) + 1);
 		try {
-			con = (HttpURLConnection) new URI(streamUrl).toURL().openConnection();
-			targetStream = con.getInputStream();
-		} catch (MalformedURLException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
+			data = readByteRange(streamUrl, rangeStart, rangeEnd);
 		} catch (IOException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 		} catch (URISyntaxException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
-		} 
-	    
-	    
-	    return targetStream;
-		
-		
+		}
+		return  data;
 	}
+
+	public byte[] readByteRange(String streamUrl, long start, long end) throws IOException, URISyntaxException {
+		try (InputStream inputStream = (new URI(streamUrl).toURL().openStream());
+				ByteArrayOutputStream bufferedOutputStream = new ByteArrayOutputStream()) {
+			byte[] data = new byte[128];
+			int nRead;
+			while ((nRead = inputStream.read(data, 0, data.length)) != -1) {
+				bufferedOutputStream.write(data, 0, nRead);
+			}
+			bufferedOutputStream.flush();
+			byte[] result = new byte[(int) (end - start) + 1];
+			System.arraycopy(bufferedOutputStream.toByteArray(), (int) start, result, 0, result.length);
+			return result;
+		}
+	}
+
+	public String getRangeStart(String rangeHeader) {
+
+		String rangeStart = null;
+
+		if (rangeHeader != null && rangeHeader.startsWith("bytes=")) {
+			String[] rangeParts = rangeHeader.substring(6).split("-");
+			rangeStart = rangeParts[0];
+			// Now you have the range start
+			System.out.println("Range start: " + rangeStart);
+		} else {
+			System.out.println("Range header not found or invalid.");
+		}
+
+		return rangeStart;
+	}
+
+	public String getRangeEnd(String rangeHeader) {
+
+		String rangeEnd = String.valueOf(ChunkSize);
+
+		if (rangeHeader != null && rangeHeader.startsWith("bytes=")) {
+			String[] rangeParts = rangeHeader.substring(6).split("-");
+			if (rangeParts.length > 1) {
+				rangeEnd = rangeParts[1];
+				// Now you have the range start
+				System.out.println("Range end: " + rangeEnd);
+			}
+		} else {
+			System.out.println("Range header not found or invalid.");
+		}
+
+		return rangeEnd;
+	}
+
 }
