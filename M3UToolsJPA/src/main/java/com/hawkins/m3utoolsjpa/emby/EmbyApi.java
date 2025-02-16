c9e686183cb9410087c89a471047108596f04932
jonathan <jonathan@CM-AMD-LINUX>
2025-02-16 12:36:12 +0000

    Removed unused imports

--- M3UToolsJPA/src/main/java/com/hawkins/m3utoolsjpa/emby/EmbyApi.java
+++ M3UToolsJPA/src/main/java/com/hawkins/m3utoolsjpa/emby/EmbyApi.java
@@ -1,8 +1,6 @@
 package com.hawkins.m3utoolsjpa.emby;
 
-import java.io.BufferedReader;
 import java.io.IOException;
-import java.io.InputStreamReader;
 import java.net.HttpURLConnection;
 import java.net.MalformedURLException;
 import java.net.URI;
