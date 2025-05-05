a702756ec840cb8173163d43661a179c6a943b9b
jonathan <jonathan@pop-os>
2025-05-05 14:06:31 +0100

    Allow h2-console to be displayed

--- M3UToolsJPA/src/main/java/com/hawkins/m3utoolsjpa/configuration/WebAppSecurityConfig.java
+++ M3UToolsJPA/src/main/java/com/hawkins/m3utoolsjpa/configuration/WebAppSecurityConfig.java
@@ -5,6 +5,7 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.HeadersConfigurer;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
@@ -44,8 +45,8 @@ public class WebAppSecurityConfig {
             .headers(headers -> {
                 headers
                     .httpStrictTransportSecurity(Customizer.withDefaults())
-                    .xssProtection(Customizer.withDefaults());
-                    
+                    .xssProtection(Customizer.withDefaults())
+                	.frameOptions(HeadersConfigurer.FrameOptionsConfig::sameOrigin);
             });
         return http.build();
     }
